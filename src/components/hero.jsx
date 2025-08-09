ec8daca678f82868cd306bb06077bc6338da0d32
Rocky Lai <r0912510@student.thomasmore.be>
2025-08-09 11:32:24 +0200

    add button back

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -13,7 +13,13 @@ const Hero = () => {
         <p style={styles.paragraph}>
           I design and build thoughtful digital products. Passionate about creating seamless user experiences.
         </p>
-          
+          <a
+  href="/Curriculum vitae.pdf"
+  download
+  className="hero-button"
+>
+  Download CV
+</a>
       </div>
     
     </section>
