dca2ff252107a0044472ca66261b29e3b4f5c118
Rocky Lai <r0912510@student.thomasmore.be>
2025-08-09 11:15:43 +0200

    useLocation toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/hero.jsx';
